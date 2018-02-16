5142b12a1a51ea8b6c58213e1adb9e67484b6287
John Suen <johnnie.suen@gmail.com>
2018-02-16 23:14:57 +1100

    Initial commit

---
+++ TestNinjaTests/Fundamentals/FizzBussTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestNinjaTests.Fundamentals
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
